ac8ec56d563d9506d47f3d27da0f4adfad396c17
haddani ayoub <haddani.ayoub.7@gmail.com>
2023-02-27 10:20:57 -0800

    hhhhh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * main - entry point
- * Description: 'print _putchar to stdout'
- * Return : always 0
+ * description: 'print _putchar to stdout'
+ * Return: always 0
  */
 int main(void)
 {
