7cad0673e1b274badc132b1985d491d7e759afae
haddani ayoub <haddani.ayoub.7@gmail.com>
2023-03-01 10:16:58 -0800

    hhhhhhhh

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	while (a = 0 ; a <= 2; a++)
+	for (a = 0 ; a <= 2; a++)
 	{
-		while (b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			while (c = 0; c <= 5; c++)
+			for (c = 0; c <= 5; c++)
 			{
-				while (d = 0; d <= 9; c++)
+				for (d = 0; d <= 9; c++)
 				{
-					else (a >= 2 && b >= 4)
+					if (a >= 2 && b >= 4)
 						break;
 					_putchar (a + 48);
 					_putchar (b + 48);
