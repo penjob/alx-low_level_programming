03eb3de6e165204caa5889e44f0c19e5aaea9077
haddani ayoub <haddani.ayoub.7@gmail.com>
2023-03-01 08:07:13 -0800

    hh

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - checks for lowercase character
  *
- * Return: 1 for lowercase chracter. 0 for the rest 
+ * Return: 1 for lowercase chracter. 0 for the rest
  */
 int _islower(int c)
 {
