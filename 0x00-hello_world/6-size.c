a625dadd6f162eec22dc5dfa138709d5b4585127
penjob <penjob07@gmail.com>
2023-07-17 13:57:04 -0700

    c is fun

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - main entry point
+ *Return: always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n ", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n ", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
