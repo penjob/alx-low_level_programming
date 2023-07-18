14ec4642a73126cbec5dcd51a8512a71ef5d0876
penjob <penjob07@gmail.com>
2023-07-18 12:36:38 -0700

    hhh

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n");
 	}
-		else if (n == 0)
-			{
-				printf("%d is zero\n", n);
-			}
-
-					else{
-					      printf("%d is negative\n", n);
-			}
+	else if (n == 0)
+	{
+		printf("%d is zero\n");
+	}
+	else
+	{
+		printf("%d is negative\n");
+	}
 	return (0);
 }
