90cb3fcbedb10a8afb76a7ada738dd70826759b0
penjob <penjob07@gmail.com>
2023-07-18 18:32:32 -0700

    hhh

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Print alphabet in lowercase using only `_putchar' x10
+ *Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x = 97;
+	int y = 1;
+
+	do {
+		while (x <= 122)
+		{
+			_putchar(x);
+		x++;
+		}
+	} while (y <= 10);
+	{
+		_putchar('\n');
+		y++;
+	}
+}
