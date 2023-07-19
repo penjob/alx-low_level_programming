d5f1ac2d6b39171dcaad33881c13a4efe306f824
penjob <penjob07@gmail.com>
2023-07-18 17:44:27 -0700

    hhh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - alpha
+ *Return: always 0
+ */
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
