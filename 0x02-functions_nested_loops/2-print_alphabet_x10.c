fd50153fdb3e1d783afa3021a1ce28b325fbc03f
penjob <penjob07@gmail.com>
2023-07-18 18:45:04 -0700

    hhh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 	int x = 97;
 	int y = 1;
 
-	do {
-		while (x <= 122)
+	while (x <= 122)
+	{
+		while (y <= 10)
 		{
 			_putchar(x);
 			x++;
 			y++;
 		}
 		_putchar('\n');
-	} while (y <= 10);
 }
