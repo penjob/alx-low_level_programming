5650b07c0890e091bd90c2236bbf8d3c1e572a1d
penjob <penjob07@gmail.com>
2023-07-20 14:45:04 -0700

    hh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_line - print a line
+ *@n: number of _
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
