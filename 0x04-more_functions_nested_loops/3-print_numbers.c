edecba17953f95c34215c0874c8b29603c9de9ea
penjob <penjob07@gmail.com>
2023-07-20 12:57:15 -0700

    hhh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - prints numbers
+ *Return: always 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
