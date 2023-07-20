7f424132395c510546423040bd8b770ffef37645
penjob <penjob07@gmail.com>
2023-07-20 13:18:42 -0700

    hhhh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 
 	for (c = 48; c <= 57; c++)
 	{
-		_putchar(c);
-		if (c == 50)
+		if (c == 50 || c == 52)
 		{
 			continue;
 		}
+		_putchar(c);
 	}
 	_putchar('\n');
 }
