7bb3833530879c27b9c4a42919bf0e1914322d82
penjob <penjob07@gmail.com>
2023-07-19 18:34:39 -0700

    hhh

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *main - ascii
+ *Return: always 0
+ */
+int main (void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = 48; y <= 57; y++)
+		{
+			for (z = 48; z <= 57; z++)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x == 55 && y == 56 && z == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
