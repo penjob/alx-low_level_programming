895b3ecf4ec3b19a06add8e27d0d0d5ff9770202
penjob <penjob07@gmail.com>
2023-07-20 17:14:17 -0700

    hhh

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,12 +5,16 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i = 0;
+	int a;
+
+	if (i >= n)
+	{
+		_putchar('\n');
+	}
 
 	for (i = 0; i < n; i++)
 	{
-		int a;
-
 		for (a = 0; a < i; a++)
 		{
 			_putchar(' ');
