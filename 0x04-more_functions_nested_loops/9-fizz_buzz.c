9ea7776228ade780d92f45a4f0d6b3c243e2fe20
penjob <penjob07@gmail.com>
2023-07-20 17:52:45 -0700

    ggg

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - fizzbuzz
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
