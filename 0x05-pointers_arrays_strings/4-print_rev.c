3b141b0ab335f40f2a39f4a6e58c0dd572bded15
penjob <penjob07@gmail.com>
2023-07-27 14:29:03 -0700

    hh

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_rev - print reverse
+ *@s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
