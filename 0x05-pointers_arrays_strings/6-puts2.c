5e3ab5e12414f1aee7f3f4aa528c7b593b7847e4
penjob <penjob07@gmail.com>
2023-07-31 12:15:00 -0700

    hh

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *puts2 - prints string elements
+ *@str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n')
+}
