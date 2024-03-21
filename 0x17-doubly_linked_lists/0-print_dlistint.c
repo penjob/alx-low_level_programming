9d8a606ec0e3ae8e6d1e846788382a9d86dc762c
penjob <penjob07gmail.com>
2024-03-21 14:46:41 -0700

    hh

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include "list.h"
+/**
+ * print_distinct - print doubly linked list
+ * @h: adress of head node
+ * Return: size of node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
