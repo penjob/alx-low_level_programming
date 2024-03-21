f8faf1470f6c65d93a509ab01870acc532137a32
penjob <penjob07gmail.com>
2024-03-21 16:09:21 -0700

    hh

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * get_dnodeint_at_index - get nth node of a dlistint_t linked list
+  * @head: header of double linked list
+  * @index: index of the node, starting from 0
+  * Return: a address of nth node
+  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if i == index
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
