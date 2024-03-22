f8e685fd5cf4c8254a6169d99d9184dc324643ee
penjob <penjob07gmail.com>
2024-03-22 16:26:53 -0700

    hh

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,16 +12,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
-		return (add_dnodeint(h. n));
+		return (add_dnodeint(h, n));
 
-	for (, idx != 1, idx--)
+	for ( ; idx != 1; idx--)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
 	}
 	if (tmp->next == NULL)
-		return (add_dnodeint_end(h. n));
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 
